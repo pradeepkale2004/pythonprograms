04f9c19842524dec8229dcf50b3da163c1f0bc06
pradeepkale2004 <pradeepkale2004@gmail.com>
2026-01-08 23:15:17 +0530

    commit 080126

---
+++ Python Intervies Codes Cheatsheet/1.py
@@ -0,0 +1,11 @@
+#Convertin an integer into a Decimal
+import decimal
+
+integer =150
+decimal0=float(integer)
+print(decimal0)
+print(type(decimal0))
+
+decimal1 = integer/1.0
+print(decimal1)
+print(type(decimal1))
